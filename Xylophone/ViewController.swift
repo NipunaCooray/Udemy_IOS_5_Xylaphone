64cfaf5dc3d13b9a31bea4aeedc0de08c1229a96
NipunaCooray <nipuna.cooray@gmail.com>
2020-02-07 11:27:38 +1100

    Completed project working xylaphone, button opacity with delay

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
     }
 
+    @IBAction func onButtonClikced(_ sender: UIButton) {
+        let note = sender.currentTitle!
+        
+        playSound(soundNote: note)
+        
+        //Reduces the sender's (the button that got pressed) opacity to half.
+        sender.alpha = 0.5
+        
+        //Code should execute after 0.2 second delay.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+        }
+        
+    }
 
+    func playSound(soundNote : String) {
+        let url = Bundle.main.url(forResource: soundNote, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+
+    }
     
 
 }
